2d1cea51738a66b102e4ec795787ac1ef3275155
shawnmcmahon <shawnmcmahon17@gmail.com>
2022-05-02 19:37:14 -0600

    Create home path and set up server to proper port

--- index.js
+++ index.js
@@ -4,4 +4,20 @@ require('dotenv').config()
 const app = express();
 const cors = require('cors')
 
-const { pool } = require('./config')
+// const { pool } = require('./config')
+
+
+app.use(express.json()); 
+app.use(cors());
+
+app.locals.title = 'Find Me Fast API'
+
+// pool.connect();
+
+app.get('/', (request, response) => {
+  response.status(200).send(`Welcome to the ${app.locals.title}`)
+}); 
+
+app.listen(3000, () => {
+  console.log('Server listening')
+})
