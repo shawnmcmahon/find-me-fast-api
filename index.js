58dace1f28700cacd54afc15af38dd6f29541730
shawnmcmahon <shawnmcmahon17@gmail.com>
2022-05-03 21:00:10 -0600

    Resolved Cors Error but now have 500 error :(

--- index.js
+++ index.js
@@ -19,14 +19,15 @@ const corsOpts = {
 
 
 app.use(express.json()); 
-app.use(cors(corsOpts));
+app.use(cors());
+// app.use(cors(corsOpts));
 
 app.locals.title = 'Find Me Fast API'
 
 // pool.connect();
 
 app.use((request, response, next) => {
-  const allowedOrigins = ['www.localhost:4000/']; 
+  const allowedOrigins = ['www.localhost:3000/']; 
   const origin = req.headers.origin; 
   if (allowedOrigins.includes(origin)) {
     response.setHeader('Access-Control-Allow-Origin', origin);
@@ -34,7 +35,11 @@ app.use((request, response, next) => {
   return next();
 })
 
-app.get('/', (request, response) => {
+app.get('*', (request, response) => {
+  response.status(200).send('Welcome to the Find Me Fast Api - Endpoint not found')
+}); 
+
+app.get('/home', (request, response) => {
   response.status(200).send('Welcome to the Find Me Fast Api')
 }); 
 
