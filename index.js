55102894c32e0fc9a432a09e9d748b10fde34fe5
shawnmcmahon <shawnmcmahon17@gmail.com>
2022-05-03 20:07:37 -0600

    Troubleshoot cors error with new use function

--- index.js
+++ index.js
@@ -6,18 +6,38 @@ const cors = require('cors')
 
 // const { pool } = require('./config')
 
+const corsOpts = {
+  origin: '*', 
+  methods: [
+    'GET', 
+    'POST'
+  ], 
+  allowedHeaders: [
+    'Content-type'
+  ]
+}
+
 
 app.use(express.json()); 
-app.use(cors());
+app.use(cors(corsOpts));
 
 app.locals.title = 'Find Me Fast API'
 
 // pool.connect();
 
+app.use((request, response, next) => {
+  const allowedOrigins = ['www.localhost:4000/']; 
+  const origin = req.headers.origin; 
+  if (allowedOrigins.includes(origin)) {
+    response.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  return next();
+})
+
 app.get('/', (request, response) => {
-  response.status(200).send(`Welcome to the ${app.locals.title}`)
+  response.status(200).send('Welcome to the Find Me Fast Api')
 }); 
 
-app.listen(3000, () => {
+app.listen(4000, () => {
   console.log('Server listening')
 })
